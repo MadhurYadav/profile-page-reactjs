2299c1342249e999909441ac8815f21a35eacf0e
Madhur Yadav <94036633+MadhurYadav@users.noreply.github.com>
2023-11-22 20:53:11 +0530

    Update Cover.jsx

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -4,7 +4,7 @@ import "./Cover.css"
 const Cover = () => {
   return (
     <div className='cover'>
-        <img src="https://c4.wallpaperflare.com/wallpaper/664/485/279/architecture-building-city-cityscape-wallpaper-preview.jpg" alt="" />
+        <img src="https://images.unsplash.com/photo-1682685794761-c8e7b2347702?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
     </div>
   )
 }
